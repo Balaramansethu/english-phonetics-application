e674e6c4b4aeb2714507e3393e5ac5c2ad99de05
Balaramansethu <baalaaraman@gmail.com>
2023-10-15 22:54:03 +0530

    Initial commit for Frontend and middleware file structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { HeaderComponent } from './components/HeaderComponent/HeaderComponent'
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <HeaderComponent/>
+    </React.Fragment>
+  )
+}
+
+export default App
